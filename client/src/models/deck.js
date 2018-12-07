7869c6d012bb26377f269db1afe507998438522c
Chris Jennings <chrisjennings555@gmail.com>
2018-12-07 16:56:51 +0000

    split deck function added and tested

--- client/src/models/deck.js
+++ client/src/models/deck.js
@@ -5,4 +5,15 @@ const Deck = function () {
 
 }
 
+Deck.prototype.splitDeck = function (deck) {
+
+  const handSize = deck.length/2;
+
+  const playerHand = deck.slice(0, handSize);
+  const computerHand = deck.slice(handSize, deck.length + 1);
+
+  return [playerHand, computerHand];
+
+};
+
 module.exports = Deck;
