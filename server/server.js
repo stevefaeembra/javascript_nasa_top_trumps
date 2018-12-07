7d1a26ee449771c788e0c6e55ba15f71392fd960
Steven Kay <stevendkay@gmail.com>
2018-12-07 16:40:26 +0000

    now we publish MVP hand on /api/exoplanets

--- server/server.js
+++ server/server.js
@@ -11,16 +11,8 @@ app.use(express.static(publicPath));
 app.use(bodyParser.json());
 
 const planetRouter = new PlanetRouter(data);
-app.use('/exoplanets', planetRouter);
+app.use('/api/exoplanets', planetRouter);
 
 app.listen(3000, function () {
   console.log('Started server');
 });
-
-const deal = function(number_per_player) {
-  // return an array of 2*number_per_player exoplanets
-  // shuffle uses a random comparator which returns [-0.5,0.5]
-  // this should shuffle things at random
-  const shuffleArray = (data) => arr.sort((planet) => Math.random() - .5);
-  return shuffleArray.slice(0,2*number_per_player);
-};
