22a1f9ae6132c18f76c93ab34d99d33f6a641a51
Steven Kay <stevendkay@gmail.com>
2018-12-07 16:33:58 +0000

    now got server passing back a hand with an array of the appropriate number of cards. Not random yet, but enough for MVP

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -6,10 +6,19 @@ const PlanetRouter = function (collection) {
 
   router.get('/', (req, res) => {
     console.log("Inside router yipee");
-    res.json({'message': 'Hello World'});
+    res.json({'hand': this.deal(collection, 1)});
   });
-  
+
   return router;
 }
+
+PlanetRouter.prototype.deal = function (data, number_per_player) {
+  // return an array of 2*number_per_player exoplanets
+  // shuffle uses a random comparator which returns [-0.5,0.5]
+  // this should shuffle things at random
+  //(data) => data.sort((planet) => Math.random() - .5);
+
+  return data.slice(0, 2*number_per_player);
+};
 
 module.exports = PlanetRouter;
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const PlanetRouter = require('./helpers/create_router.js');
 const data = require('./db/planets.js');
 
 const publicPath = path.join(__dirname, '../client/public');
+
 app.use(express.static(publicPath));
 app.use(bodyParser.json());
 
@@ -15,3 +16,11 @@ app.use('/exoplanets', planetRouter);
 app.listen(3000, function () {
   console.log('Started server');
 });
+
+const deal = function(number_per_player) {
+  // return an array of 2*number_per_player exoplanets
+  // shuffle uses a random comparator which returns [-0.5,0.5]
+  // this should shuffle things at random
+  const shuffleArray = (data) => arr.sort((planet) => Math.random() - .5);
+  return shuffleArray.slice(0,2*number_per_player);
+};
