42541002370acdeb46adddc18bde03a90c210641
Steven Kay <stevendkay@gmail.com>
2018-12-08 13:48:24 +0000

    server can now deal any deck size, specified in url route

--- server/src/helpers/create_router.js
+++ server/src/helpers/create_router.js
@@ -5,9 +5,17 @@ const PlanetRouter = function (collection) {
   const router = express.Router();
   console.log("Making router");
 
+  // mvp - pass back 1 card per player
+
   router.get('/', (req, res) => {
     croupier = new Croupier(collection);
     res.json(croupier.deal(1));
+  });
+
+  router.get('/deal/:number', (req, res) => {
+    croupier = new Croupier(collection);
+    var count_per_player = parseInt(req.params.number);
+    res.json(croupier.deal(count_per_player));
   });
 
   return router;
