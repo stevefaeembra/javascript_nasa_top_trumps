816ac09030abfe17a8209939a06a067b10c768ca
Chris Jennings <chrisjennings555@gmail.com>
2018-12-09 16:35:50 +0000

    initial file setup, custom element function

---
+++ client/src/views/card_view.js
@@ -0,0 +1,10 @@
+const CardView = function (container) {
+  this.container = container;
+};
+
+CardView.prototype.createCustomElement = function (elementType, label, textContent) {
+  const customElement = document.createElement(elementType);
+  customElement.textContent = `${label}: ${textContent}`;
+
+  return customElement;
+};
