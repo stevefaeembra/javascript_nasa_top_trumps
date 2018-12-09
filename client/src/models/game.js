e86930b2fda254411afcce0e15dc69ddcbd99ed1
Castlecelts <jamesdrmundie@gmail.com>
2018-12-09 09:56:49 +0000

    minor syntax change

--- client/src/models/game.js
+++ client/src/models/game.js
@@ -4,6 +4,6 @@ const RequestHelper = require('../helpers/request_helper.js');
 const Game = function (players) {
   this.currentPlayers = players;
   this.deck = null;
-}
+};
 
 module.exports = Game;
