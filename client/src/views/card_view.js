501f2aad69e38b921e12b98be7d90b6a4631ea17
Chris Jennings <chrisjennings555@gmail.com>
2018-12-09 16:45:41 +0000

    edit to render

--- client/src/views/card_view.js
+++ client/src/views/card_view.js
@@ -2,6 +2,13 @@ const CardView = function (container) {
   this.container = container;
 };
 
+CardView.prototype.renderCardDetails = function (card) {
+  const cardContainer = document.createElement('div');
+  cardContainer.id = 'card';
+
+
+};
+
 CardView.prototype.createCustomElement = function (elementType, label, textContent) {
   const customElement = document.createElement(elementType);
   customElement.textContent = `${label}: ${textContent}`;
