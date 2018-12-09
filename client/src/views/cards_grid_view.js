1850923bd48a34d40f85ef2cee904e778a2bb28f
Castlecelts <jamesdrmundie@gmail.com>
2018-12-09 11:38:54 +0000

    gridView bind events written

--- client/src/views/cards_grid_view.js
+++ client/src/views/cards_grid_view.js
@@ -1,8 +1,14 @@
 const PubSub = require('../helpers/pub_sub.js');
 const CardView = require('./card_view.js');
 
-const CardGridView = function (){
+const CardGridView = function (container){
+  this.container = container;
+};
 
+CardGridView.prototype.bindEvents = function () {
+  PubSub.subcribe('Deck:drawn-cards', (event) => {
+
+  })
 };
 
 module.exports = CardGridView;
