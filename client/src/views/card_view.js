761267ee401a89837129a45804a505d18cc60f6a
Chris Jennings <chrisjennings555@gmail.com>
2018-12-10 10:57:50 +0000

    planet stats and image ready

--- client/src/views/card_view.js
+++ client/src/views/card_view.js
@@ -9,6 +9,13 @@ CardView.prototype.renderCardDetails = function (card) {
   const image = this.createCustomElement('div', 'className', 'planet-image');
   const stats = this.createCustomElement('div', 'className', 'stats');
 
+  const imageDiv = this.createCustomElement('img', 'src', './images/earth.jpg');
+
+  image.appendChild(imageDiv);
+
+  const statsDiv = this.createCustomElement('div', 'className', 'stats-row');
+  //<img src="./images/earth.jpg" alt="earth" height='300px' width='300px'>
+
   playerCard.appendChild(image);
   playerCard.appendChild(stats);
 
