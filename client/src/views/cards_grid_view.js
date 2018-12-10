6dd2acd640c7248193c73f0d52d9d7a52c871e51
Castlecelts <jamesdrmundie@gmail.com>
2018-12-10 10:32:52 +0000

    added renderCards & createCardItem

--- client/src/views/cards_grid_view.js
+++ client/src/views/cards_grid_view.js
@@ -8,7 +8,7 @@ const CardGridView = function (container){
 CardGridView.prototype.bindEvents = function () {
   PubSub.subcribe('Deck:drawn-cards', (event) => {
     this.clearGrid();
-    this.renderCardDetails(event.detail)
+    this.renderCards(event.detail)
   });
 };
 
@@ -16,11 +16,17 @@ CardGridView.prototype.clearGrid = function () {
   this.container.innerHTML = '';
 };
 
-CardGridView.prototype.renderCardDetails = function (cards) {
+CardGridView.prototype.renderCards = function (cards) {
   cards.forEach((card) => {
     const cardItem = this.createCardItem(card);
     this.container.appendChild(cardItem);
   });
+};
+
+CardGridView.prototype.createCardItem = function (card) {
+  const cardView = new CardView();
+  const cardItem = CardView.renderCardDetails(card);
+  return cardItem;
 };
 
 module.exports = CardGridView;
