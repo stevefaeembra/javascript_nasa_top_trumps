eb84fa3555e920bcc023dac2d7bff4466a6a4317
Christopher Milligan <milliganjchristopher@gmail.com>
2018-12-11 09:53:31 +0000

    playMatch added

--- client/src/models/game.js
+++ client/src/models/game.js
@@ -10,13 +10,8 @@ const Game = function (players) {
 
 Game.prototype.bindEvents = function () {
   PubSub.subscribe('Deck:deck-changed', (event) => {
-    const myhands = event.detail;
-    const cardsInPlay = this.deck.popCardsForPlayers(myhands);
-    const categories = this.getCategories(cardsInPlay[0]);
-    const randomCategory = this.randomCategory(categories);
-    const winner = this.compareCards(cardsInPlay, randomCategory);
-    this.deck.putCardsAtBackOfHands(winner);
-    PubSub.publish('Game:winner-determined', winner);
+    const myHands = event.detail;
+    this.playMatch(myHands);
   });
 };
 
@@ -54,6 +49,16 @@ Game.prototype.compareCards = function (cards, category) {
     }
   };
   return cards.indexOf(winnerCard[0])+1;
+};
+
+Game.prototype.playMatch = function (hands) {
+  const cardsInPlay = this.deck.popCardsForPlayers(hands);
+  const categories = this.getCategories(cardsInPlay[0]);
+  const randomCategory = this.randomCategory(categories);
+  const winner = this.compareCards(cardsInPlay, randomCategory);
+  this.deck.putCardsAtBackOfHands(winner);
+  PubSub.publish('Game:hands-after-match', [this.deck.hands[0].length, this.deck.hands[1].length]);
+  PubSub.publish('Game:winner-determined', winner);
 };
 
 
