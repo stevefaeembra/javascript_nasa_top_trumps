e7f74c00dbc869b84564419457d2ed605fa29906
Castlecelts <jamesdrmundie@gmail.com>
2018-12-11 16:08:11 +0000

    styled inner content of rules modal

--- client/src/views/rules_view.js
+++ client/src/views/rules_view.js
@@ -1,7 +1,7 @@
 const PubSub = require('../helpers/pub_sub.js');
 
 const RulesView = function (element){
-  this.element = element;
+  this.element = element
 };
 
 
@@ -16,14 +16,27 @@ RulesView.prototype.renderModal = function () {
   this.element.style.visibility = 'visible';
 
   const rulesContent = document.createElement('div');
-  rulesContent.className = 'rules-modal';
-  rulesContent.style.display = 'visible';
+  rulesContent.className = 'rules-content';
 
-  const rulesText = document.createElement('div');
-  rulesText.className = 'rules-text';
-  rulesText.textContent = 'Please see this message!!';
+  const rules = [
+    "Rules",
+    "The objective of the game is to win all of your opponent’s cards, leaving them with a hand of 0.",
+    "Each match begins by each player taking the top card from their hand. The current player then chooses a category on their card.",
+    "If the selected category has a higher number than the opponent’s card’s equivalent category then the current player wins the match and adds both played cards to the back of their hand.",
+    "If the selected category value is the same for both players it is a DRAW! In this case both players discard their match card and move onto the next match.",
+    "The next match then starts, this time with the opponent getting to choose the category. This cycle continues until one player has all of the playable cards."
+  ];
 
-  rulesContent.appendChild(rulesText);
+  const rulesBody = document.createElement('div');
+  rulesBody.className = 'rulesBody';
+  rules.forEach((line) => {
+    const newLine = document.createElement('p');
+    newLine.className = 'newLine';
+    newLine.textContent = line;
+    rulesBody.appendChild(newLine);
+  })
+
+  rulesContent.appendChild(rulesBody)
   this.element.appendChild(rulesContent);
 };
 
