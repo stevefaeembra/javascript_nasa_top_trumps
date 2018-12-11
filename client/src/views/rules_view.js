11d516df114c7687802f5fdb65a78ad45629ac93
Castlecelts <jamesdrmundie@gmail.com>
2018-12-11 13:34:22 +0000

    add event listener

--- client/src/views/rules_view.js
+++ client/src/views/rules_view.js
@@ -1,7 +1,13 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const RulesView = function (){
+const RulesView = function (element){
+  this.element = element
+};
 
+RulesView.prototype.bindEvents = function () {
+  this.element.addEventListener("click", (event) => {
+    PubSub.publish("Rules:show-rules",{});
+  });
 };
 
 module.exports = RulesView;
