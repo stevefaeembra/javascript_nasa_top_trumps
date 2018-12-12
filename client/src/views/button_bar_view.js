cb1a3b7883354089abee9613e293bd8bf7bf6425
Steven Kay <stevendkay@gmail.com>
2018-12-12 11:00:22 +0000

    rename view to buttonBarView

--- client/src/views/button_bar_view.js
+++ client/src/views/button_bar_view.js
@@ -1,13 +1,13 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const NextMatchButtonView = function (element) {
+const ButtonBarView = function (element) {
   this.element = element;
 };
 
-NextMatchButtonView.prototype.bindEvents = function () {
+ButtonBarView.prototype.bindEvents = function () {
   this.element.addEventListener("click", (event) => {
     PubSub.publish("NextMatchButton:start-next-match",{});
   });
 };
 
-module.exports = NextMatchButtonView;
+module.exports = ButtonBarView;
